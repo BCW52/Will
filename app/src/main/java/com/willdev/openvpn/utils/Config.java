f555b8ee9efa1e599023514b00c8fecf937dc2ed
bcw52 <nahidislam420gamer@gmail.com>
2023-11-17 18:48:19 +0600

    200

--- app/src/main/java/com/willdev/openvpn/utils/Config.java
+++ app/src/main/java/com/willdev/openvpn/utils/Config.java
@@ -10,7 +10,7 @@ public class Config {
     public static boolean vip_subscription = false;
     public static boolean all_subscription = false;
 
-    public static String ONESIGNAL_APP_ID = "1fc314b4-cd04-4584-83f6-b1bc03cacc00";
+    public static String ONESIGNAL_APP_ID = "85110c1b-b99e-4a03-a8a3-10fd6c76903c";
 
 }
 
